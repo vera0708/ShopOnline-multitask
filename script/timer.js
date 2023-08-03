80619b8e9c96d3faa7e333ce2bc96f57edd1f5d1
vera0708 <verasdee@gmail.com>
2023-08-03 11:20:59 +0700

    feat: done task 2 lesson 2 module 6

--- script/timer.js
+++ script/timer.js
@@ -4,35 +4,40 @@ const timer = () => {
     let deadL = document.querySelector('[data-timer-deadline]');
     let deadLine = deadL.dataset.timerDeadline;
     const bannerTimer = document.querySelector('.banner__timer-time');
-    const timerDay = document.querySelector('.banner__timer-day');
-    const timerHour = document.querySelector('.banner__timer-hour');
-    const timerMin = document.querySelector('.banner__timer-min');
-
-    const timerSec = document.createElement('p');
-    const timeSecCount = document.createElement('span');
-    timerSec.classList.add('.banner__timer-sec');
-    timeSecCount.classList.add('.banner__timer-big');
-    timerSec.textContent = 'секунд';
-    timeSecCount.textContent = '00';
-    timerSec.append(timeSecCount);
+    const timerFirst = document.querySelector('.banner__timer-day');
+    const timerSecond = document.querySelector('.banner__timer-hour');
+    const timerThird = document.querySelector('.banner__timer-min');
 
     console.log('deadLine: ', deadLine);
 
     const getTimeRemaning = () => {
         const dataStop = new Date(deadLine).getTime();
-        const dataStopX = new Date(deadLine);
-        console.log(' dataStopX: ', dataStopX.getTimezoneOffset());
-        const today = Date.now();
 
-        const timeRemaining = ((dataStop - today) / 1000);
+        const dataStopDate = new Date(deadLine);
+        const dataStopGMT = dataStop - ((dataStopDate.getTimezoneOffset()) * 60 * 1000);
+        // const dataStopGMText = dataStopX.toGMTString();
 
-        const seconds = Math.floor(timeRemaining % 60);
-        const minutes = addZero(Math.floor((timeRemaining / 60) % 60));
-        const hours = addZero(Math.floor((timeRemaining / 60 / 60) % 24));
-        const days = addZero(Math.floor((timeRemaining / 60 / 60 / 24) % 30));
+        // const today = Date.now();
+        const today = new Date();
+
+        // Расчёт по местному времени:
+        // const timeRemaining = ((dataStop - today) / 1000);
+        // console.log('timeRemaining: ', timeRemaining);
+        // const seconds = Math.floor(timeRemaining % 60);
+        // const minutes = addZero(Math.floor((timeRemaining / 60) % 60));
+        // const hours = addZero(Math.floor((timeRemaining / 60 / 60) % 24));
+        // const days = addZero(Math.floor((timeRemaining / 60 / 60 / 24) % 30));
         // const months = Math.floor((timeRemaining / 60 / 60 / 24 / 30) % 12);
+
+        // Расчёт по Гринвичу :
+        const timeRemainingGMT = ((dataStopGMT - today) / 1000);
+        const seconds = Math.floor(timeRemainingGMT % 60);
+        const minutes = addZero(Math.floor((timeRemainingGMT / 60) % 60));
+        const hours = addZero(Math.floor((timeRemainingGMT / 60 / 60) % 24));
+        const days = addZero(Math.floor((timeRemainingGMT / 60 / 60 / 24) % 30));
+
         console.log(days, hours, minutes);
-        return { timeRemaining, minutes, hours, days, seconds }
+        return { timeRemainingGMT, minutes, hours, days, seconds }
     }
 
     const startTimer = () => {
@@ -44,18 +49,18 @@ const timer = () => {
         const secContent = declOfNum(timer.seconds, ['секунда', 'секунды', 'секунд']);
 
         if (timer.days >= 1) {
-            timerDay.textContent = dayContent;
-            timerHour.textContent = hourContent;
-            timerMin.textContent = minContent;
+            timerFirst.textContent = dayContent;
+            timerSecond.textContent = hourContent;
+            timerThird.textContent = minContent;
         } else {
-            timerDay.textContent = hourContent;
-            timerHour.textContent = minContent;
-            timerMin.textContent = secContent;
+            timerFirst.textContent = hourContent;
+            timerSecond.textContent = minContent;
+            timerThird.textContent = secContent;
         }
 
         const intervalId = setTimeout(startTimer, 1000);
 
-        if (timer.timeRemaining <= 0) {
+        if (timer.timeRemainingGMT <= 0) {
             clearTimeout(intervalId);
             bannerTimer.remove();
         }
