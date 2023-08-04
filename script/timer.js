227d22f13a5370bb62147cd3ae9b3a4e468cff91
vera0708 <verasdee@gmail.com>
2023-08-04 15:54:08 +0700

    feat: create scriptTimer

--- script/timer.js
+++ script/timer.js
@@ -1,10 +1,10 @@
-// import { addZero, declOfNum } from "./utilites.js";
-const declOfNum = (n, titles) => n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
-    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+import { addZero, declOfNum } from "./utilites.js";
+// const declOfNum = (n, titles) => n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+//     0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
 
-const addZero = (n) => n < 10 ? '0' + n : n;
+// const addZero = (n) => n < 10 ? '0' + n : n;
 
-const timer = () => {
+export const timer = () => {
     let deadL = document.querySelector('[data-timer-deadline]');
     let deadLine = deadL.dataset.timerDeadline;
     const bannerTimer = document.querySelector('.banner__timer');
@@ -72,5 +72,5 @@ const timer = () => {
     startTimer();
 };
 
-timer();
+// timer();
 // timer('August 7 2023 00:00:00');
